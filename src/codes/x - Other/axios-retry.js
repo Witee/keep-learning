09d693ff1279017817977bfb3cbe6b214b7d9615
Witee <xieweiting@admaster.com.cn>
2019-08-19 15:11:33 +0800

    axios retry

---
+++ src/codes/x - Other/axios-retry.js
@@ -0,0 +1,49 @@
+/**
+  axios retry
+
+  @author Witee<github.com/Witee>
+  @date   2019-08-19
+*/
+
+const axios = require('axios');
+const _ = require('lodash');
+const async = require('async');
+
+const axiosRetry = {
+  post: (url, data, config) => {
+    const api = (cb) => {
+      axios
+        .post(url, data, config)
+        .then((res) => {
+          console.log('post success');
+          cb(null, res);
+        })
+        .catch((err) => {
+          console.log('post failed');
+          cb(err);
+        });
+    };
+
+    return new Promise((resolve, reject) => {
+      const retryOpt = {
+        times: 3,
+        errorFilter: (err) => {
+          const status = _.parseInt(_.get(err, 'response.status', 500)); // 只重试指定返回码的请求
+          return status >= 500;
+        },
+      };
+
+      async.retry(retryOpt, api, (err, result) => {
+        if (err) reject(err);
+
+        resolve(result);
+      });
+    });
+  },
+};
+
+(async () => {
+  const res = await axiosRetry.post('http://127.0.0.1:9527/test');
+
+  console.log('res: ', res.data);
+})();
--- src/codes/x - Other/retry.js
+++ src/codes/x - Other/retry.js
@@ -2,7 +2,7 @@
   async.retry 用法
 
   api 为异步函数，
-  成功时调用 cb 函数，将第一个参数设置为 null，第二个参数为数据；
+  成功时调用 callback 函数，将第一个参数设置为 null，第二个参数为数据；
   失败时第一个参数设置为出错信息，即可实现重试
 
   @author Witee<github.com/Witee>
@@ -10,20 +10,7 @@
 */
 
 const async = require('async');
-
-const api = (cb) => {
-  const timeOut = Math.random() * 2;
-  setTimeout(() => {
-    if (timeOut < 1) {
-      console.log('call success');
-      cb(null, '200 OK');
-    } else {
-      console.log('call failed');
-      cb('api error');
-    }
-  }, timeOut * 1000);
-};
-
+const _ = require('lodash');
 const retryOpt = {
   times: 3,
   errorFilter: (err) => {
@@ -31,7 +18,53 @@ const retryOpt = {
     return status >= 500;
   },
 };
+
+const api = (callback) => {
+  const timeOut = Math.random() * 2;
+  setTimeout(() => {
+    if (timeOut < 1) {
+      console.log('call success');
+      callback(null, '200 OK');
+    } else {
+      console.log('call failed');
+      callback('api error');
+    }
+  }, timeOut * 1000);
+};
+
 async.retry(retryOpt, api, (err, result) => {
+  if (err) {
+    console.log('err: ', err);
+  } else {
+    console.log('result: ', result);
+  }
+});
+
+// 如果 api 返回 promise，则会报 TypeError: callback is not a function
+// 此时需要使用 promise 的 then() 方法定义 api
+
+const api2 = (callback) => {
+  const timeOut = Math.random() * 2;
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (timeOut < 1) {
+        resolve();
+      } else {
+        reject();
+      }
+    }, timeOut * 1000);
+  })
+    .then(() => {
+      console.log('api2 call success');
+      callback(null, 'api2 200 OK');
+    })
+    .catch(() => {
+      console.log('api2 call failed');
+      callback('api2 api error');
+    });
+};
+
+async.retry(retryOpt, api2, (err, result) => {
   if (err) {
     console.log('err: ', err);
   } else {
